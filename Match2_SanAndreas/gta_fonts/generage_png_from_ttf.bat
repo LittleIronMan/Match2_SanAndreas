aeaab497d51ecc8821ddd38f6df8c9049bef4786
LittleIronMan <mrstark375@gmail.com>
2020-08-15 14:13:13 +0700

    Сцена меню, черновик
    Добавил шрифты из GTA

---
+++ Match2_SanAndreas/gta_fonts/generage_png_from_ttf.bat
@@ -0,0 +1,3 @@
+:: "C:\Program Files\ImageMagick-7.0.10-Q16-HDRI\magick.exe" --help
+"C:\Program Files\ImageMagick-7.0.10-Q16-HDRI\magick.exe" -background none -fill white -font San_Andreas_Title(diploma).ttf -pointsize 80 label:"San Andreas" San_Andreas.png
+pause
